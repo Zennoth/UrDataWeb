0464f7d8d4b33ba397de48d9fbe1f60d894b25a6
Zennoth <62875502+Zennoth@users.noreply.github.com>
2021-01-19 15:15:52 +0700

    add seeder

---
+++ database/seeders/LecturerSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Lecturer;
+use Illuminate\Support\Facades\Hash;
+
+class LecturerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    }
+}
